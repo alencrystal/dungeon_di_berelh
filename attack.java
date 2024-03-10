7c3526dcc794dbd353e761d999392b62e30745c5
alencrystal <153738740+alencrystal@users.noreply.github.com>
2024-03-10 22:11:09 +0100

    update_2

--- attack.java
+++ attack.java
@@ -8,30 +8,4 @@ public class attack {
         return rand1 + player.AD * a + player.AP * b - enemy.def * c - enemy.mres * d;
     }
 
-        /*public static int first(){
-
-            Random rand = new Random();
-            int hit = rand.nextInt(2, 6);
-            return hit + player.AP - enemy.mres;
-        }
-        public static int second(){
-
-            Random rand = new Random();
-            int hit = rand.nextInt(2, 6);
-            return hit + player.AP + (2 * player.AD) - enemy.mres - enemy.def;
-        }
-        public static int third(){
-
-            Random rand = new Random();
-            int hit = rand.nextInt(4, 9);
-            return hit + player.AP - enemy.mres - enemy.def;
-        }
-        public static int fourth(){
-
-            Random rand = new Random();
-            int hit = rand.nextInt(1, 5);
-            return hit + (2 * player.AP) - enemy.mres - enemy.def;
-        }  
-    }
-    */
 }
