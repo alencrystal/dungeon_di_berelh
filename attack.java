f59370d2b23f8304adee4dffd22e829a0031cb27
alencrystal <153738740+alencrystal@users.noreply.github.com>
2024-03-16 15:06:28 +0100

    update_11

--- attack.java
+++ attack.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 public class attack {
     
     public static int dmg(int a, int b, int c, int d){
@@ -10,6 +11,53 @@ public class attack {
             hit = 0;
         }
         return hit;
+    }
+
+
+    public static void select(){
+
+        boolean syntax = false;
+        Scanner sc=new Scanner(System.in);
+        
+        while (syntax == false) {
+
+            System. out. print("\033[H\033[2J");
+            System.out.println("scegli che attacco eserguire");
+            String lavoro = sc.nextLine();
+            
+            switch (lavoro) {
+
+                case "1":
+                    attack.dmg(1, 0, 1, 0);
+                    syntax = true;
+                    break;
+                case "2":
+                    attack.dmg(1, 0, 1, 0);
+                    syntax = true;
+                    break;
+                case "3":
+                    attack.dmg(1, 0, 1, 0);
+                    syntax = true;
+                    break;
+                case "4":
+                    attack.dmg(1, 0, 1, 0);
+                    syntax = true;
+                    break;
+                case "5":
+                    attack.dmg(1, 0, 1, 0);
+                    syntax = true;
+                    break;
+                case "6":
+                    attack.dmg(1, 0, 1, 0);
+                    syntax = true;
+                    break;
+                default:
+                    break;
+                
+            }
+            
+        }
+        System. out. print("\033[H\033[2J");   
     }
 
 }
